24a9151b87f8a60797fff83fbba6267932e5eb7a
Eddy Caldas <eddycaldas@live.com>
2019-05-04 18:29:26 -0600

    queries, db, and routes

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8080
 
+const routes = {
+  knowledgearticle: require('./routes/knowledgearticle')
+}
+
+
+app.use('/knowledgearticle', routes.knowledgearticle)
+
 app.listen(PORT, () => console.log(`port ${PORT}`))
---
+++ db/queries.js
@@ -0,0 +1,21 @@
+const knex = require('./')
+
+module.exports = {
+  knowledgearticle: {
+    // delete(id) {
+    //   return knex('knowledgearticle').where({id}).delete()
+    // },
+    // create(body) {
+    //   return knex('knowledgearticle').insert(body)
+    // },
+    // read(id) {
+    //   return knex('knowledgearticle').where({id}).first()
+    // },
+    // update(id, body) {
+    //   return knex("knowledgearticle").where({id}).update(body)
+    // },
+    list() {
+      return knex('knowledgearticle')
+    },
+  }
+}
