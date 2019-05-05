b3377af6d7149f794c185bd1b02f1d02fb1fd3b1
Eddy Caldas <eddycaldas@live.com>
2019-05-05 14:06:35 -0600

    update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const PORT = process.env.PORT || 8080
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
