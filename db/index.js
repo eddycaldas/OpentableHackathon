a21bef0153ad3b0a0f55da4577ac3781f119b962
Eddy Caldas <eddycaldas@live.com>
2019-05-06 14:40:01 -0600

    update index.js

--- db/index.js
+++ db/index.js
@@ -1,12 +1,6 @@
-// const knex = require('knex')
-// const config = require('../knexfile')
-// const env = process.env.NODE_ENV || 'development'
-// 
-// module.exports = knex(config[env])
-
-const environment = process.env.NODE_ENV || 'development';
+const knex = require('knex')
 const config = require('../knexfile')
-const environmentConfig = config[environment];
-const knex = require('knex');
-const connection = knex(environmentConfig);
-module.exports = connection;
+const env = process.env.NODE_ENV || 'development'
+
+module.exports = knex(config[env])
+
